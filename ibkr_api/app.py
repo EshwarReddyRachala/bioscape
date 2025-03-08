b2c45800435f9fbb94d7e213c43f68caf6335bf4
Eshwar Rachala <rachala.2000@gmail.com>
2025-03-08 05:04:49 +0000

    added home route

--- ibkr_api/app.py
+++ ibkr_api/app.py
@@ -2,9 +2,11 @@ from flask import Flask
 from setuptools import setup, find_packages
 from ibkr_api.routes.order_routes import order_bp
 from ibkr_api.routes.status_routes import status_bp
+from ibkr_api.routes.home_routes import home_bp
 
 def create_app():
     app = Flask(__name__)
+    app.register_blueprint(home_bp)
     app.register_blueprint(order_bp)
     app.register_blueprint(status_bp)
     return app
