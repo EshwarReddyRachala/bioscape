de84cb17af9bbdb617026da04a84a645b5875e96
Eshwar Rachala <rachala.2000@gmail.com>
2025-03-08 05:14:22 +0000

    updated

--- ibkr_api/app.py
+++ ibkr_api/app.py
@@ -6,7 +6,7 @@ from ibkr_api.routes.home_routes import home_bp
 
 def create_app():
     app = Flask(__name__)
-    app.register_blueprint(home_bp)
-    app.register_blueprint(order_bp)
-    app.register_blueprint(status_bp)
+    app.register_blueprint(home_bp, url_prefix="/api")
+    app.register_blueprint(order_bp, url_prefix="/api")
+    app.register_blueprint(status_bp, url_prefix="/api")
     return app
